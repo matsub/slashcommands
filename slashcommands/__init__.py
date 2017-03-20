2f0e236673685a145e6861a519ffe4718bd9c7ce
matsub <matsub.rk@gmail.com>
2017-03-20 16:43:57 +0900

    Make slashcommands as a package

---
+++ slashcommands/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+    slashcommands
+    =====
+    A tiny framework for slash commands of Slack.
+
+    Repository: https://github.com/matsub/slashcommands
+    License: MIT
+'''
+
+__author__ = 'matsub'
+__version__ = '0.1'
+__license__ = 'MIT'
+
+from .slashcommands import SlashCommands
