44a0622d913f8de37ba43b6a07d9d5e20d3f6c36
matsub <matsub.rk@gmail.com>
2017-03-20 23:32:11 +0900

    add error test

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,1 +1,22 @@
 # coding: utf-8
+
+import unittest
+from slashcommands import SlashCommands
+from slashcommands.errors import RouteDuplicated
+
+
+class TestErrors(unittest.TestCase):
+
+    def setUp(self):
+        self.TOKEN = 'test'
+        self.app = SlashCommands(self.TOKEN)
+
+    def test_route_duplicate(self):
+        with self.assertRaises(RouteDuplicated):
+            @self.app.route('/route')
+            def route1(body):
+                return None
+
+            @self.app.route('/route')
+            def route2(body):
+                return None
