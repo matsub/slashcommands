4870d4b1ce47389d151d79bea5bfec854870801e
matsub <matsub.rk@gmail.com>
2017-03-20 23:31:57 +0900

    add add basic test

--- tests/test_slashcommands.py
+++ tests/test_slashcommands.py
@@ -1,1 +1,31 @@
 # coding: utf-8
+
+import unittest
+from slashcommands import SlashCommands
+
+
+class TestSlashCommands(unittest.TestCase):
+
+    def setUp(self):
+        self.TOKEN = 'test'
+        self.app = SlashCommands(self.TOKEN, prefix='/main/')
+        self.subapp = SlashCommands(self.TOKEN, prefix='/sub/')
+
+    def test_token_validation(self):
+        validated = self.app.is_valid(self.TOKEN)
+        self.assertTrue(validated)
+
+    def test_add_route(self):
+        @self.app.route('/test')
+        def test_route(body):
+            return None
+
+        self.assertTrue('/main/test' in self.app.routes)
+
+    def test_install_app(self):
+        @self.subapp.route('/test')
+        def test_route(body):
+            return None
+
+        self.app.install(self.subapp)
+        self.assertTrue('/sub/test' in self.app.routes)
