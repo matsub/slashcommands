b734dcfeb5a60d7043c38d5bc7b2e9c885f9e929
matsub <matsub.rk@gmail.com>
2017-03-20 23:31:32 +0900

    add response test

--- tests/test_response.py
+++ tests/test_response.py
@@ -1,1 +1,80 @@
 # coding: utf-8
+
+import unittest
+import json
+import urllib.error
+from .webutil import Client
+from .testserver import TOKEN
+
+
+class TestResponse(unittest.TestCase):
+    """\
+    Basic Testings for SlashCommands
+
+    - Router works correctly
+    - Application inheritance
+    - 404 Not found
+    - 405 Method Not allowed
+    - Invalid Request
+    - Invalid Token
+    """
+
+    def setUp(self):
+        self.client = Client('http://localhost:8080/')
+
+    def test_post(self):
+        data = {'token': TOKEN}
+        expectations = [
+            ('/hello/', "hello!"),
+            ('/hello/foo', "foo!"),
+            ('/sub/', "I'm sub app!"),
+        ]
+
+        for url, ans in expectations:
+            res = self.client.post(url, data)
+            self.assertEqual(res.status, 200)
+            self.assertEqual(res.read().decode(), ans)
+
+    def test_404(self):
+        data = {'token': TOKEN}
+        url = '/url/not/exist'
+
+        try:
+            res = self.client.post(url, data)
+        except urllib.error.HTTPError as e:
+            self.assertEqual(e.code, 404)
+
+    def test_405(self):
+        data = {'token': TOKEN}
+        url = '/hello/'
+
+        try:
+            res = self.client.get(url)
+        except urllib.error.HTTPError as e:
+            self.assertEqual(e.code, 405)
+
+    def test_InvalidRequest(self):
+        data = {'foo': 'bar'}
+        url = '/hello/'
+
+        try:
+            res = self.client.post(url, data)
+        except urllib.error.HTTPError as e:
+            self.assertEqual(e.code, 400)
+            res_json = json.load(e)
+            self.assertTrue('detail' in res_json)
+            msg = "This request is not following Slash command format."
+            self.assertEqual(res_json['detail'], msg)
+
+    def test_InvalidToken(self):
+        data = {'token': 'invalid'+TOKEN}
+        url = '/hello/'
+
+        try:
+            res = self.client.post(url, data)
+        except urllib.error.HTTPError as e:
+            self.assertEqual(e.code, 400)
+            res_json = json.load(e)
+            self.assertTrue('detail' in res_json)
+            msg = "Token doesn't match."
+            self.assertEqual(res_json['detail'], msg)
---
+++ tests/testserver.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+sys.path.append('../slashcommands')
+
+from slashcommands import SlashCommands
+
+TOKEN = 'test'
+app = SlashCommands(TOKEN, prefix='/hello/')
+subapp = SlashCommands(TOKEN, prefix='/sub/')
+
+
+@app.route('/')
+def hello(body):
+    return "hello!"
+
+
+@app.route('/foo')
+def hello2(body):
+    return "foo!"
+
+
+@subapp.route('/')
+def foo(body):
+    return "I'm sub app!"
+
+
+app.install(subapp)
+
+if __name__ == '__main__':
+    app.run(port=8080)
